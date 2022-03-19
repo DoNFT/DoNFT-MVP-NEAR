77aceca3acd1234c11246264158f2bbe17497fbe
Arsenii Pechenkin <arspechen@gmail.com>
2022-03-20 01:09:04 +0300

    [near] fix deploy script, add readme

---
+++ crypto/near/README.md
@@ -0,0 +1,16 @@
+# DoNFT MVP for NEAR Smart Contract
+
+Based on https://docs.near.org/docs/tutorials/contracts/nfts/introduction#
+
+Aditional functions:
+- bundling NFTs
+- unbundling NFT
+
+## Build smart contract
+
+npm run build
+
+## Deploy smart contract
+
+Set up .env 
+npm run deploy --env=.env
--- crypto/near/deploy.sh
+++ crypto/near/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+export $(grep -v '^#' .env | xargs)
+
 set -e && RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release && mkdir -p ./out && cp target/wasm32-unknown-unknown/release/*.wasm ./out/main.wasm
+
 near login
-near create-account nft-list.near_testing.testnet --masterAccount near_testing.testnet --initialBalance 10
-NFT_CONTRACT_ID=nft-list.near_testing.testnet
-MAIN_ACCOUNT=near_testing.testnet
-echo $NFT_CONTRACT_ID
-echo $MAIN_ACCOUNT
+
+near create-account $NFT_CONTRACT_ID --masterAccount $MAIN_ACCOUNT --initialBalance 10
 near deploy --accountId $NFT_CONTRACT_ID --wasmFile out/main.wasm
 near call $NFT_CONTRACT_ID new_default_meta '{"owner_id": "'$NFT_CONTRACT_ID'"}'  --accountId $NFT_CONTRACT_ID 
 near view $NFT_CONTRACT_ID nft_metadata
